2fa71feec1cd466bfed5386f7abff69623046dd4
Kevin Li <kevinli200555@gmail.com>
2025-07-03 18:24:48 -0500

    Commented

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-//Just a class containing color themes to keep the theme consistent
+// Just a class containing color themes to keep the theme consistent
 class ColorTheme {
   static Color primary = const Color.fromARGB(255, 5, 50, 90);
   static Color accent = const Color.fromARGB(255, 40, 28, 165);
@@ -9,4 +9,4 @@ class ColorTheme {
   static Color alternateTextColor = Colors.white;
   static Color red = const Color.fromARGB(255, 210, 5, 5);
   static Color green =  const Color.fromARGB(255, 16, 159, 21);
-}
+} // EOF colors.dart
