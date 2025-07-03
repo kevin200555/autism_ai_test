672e20b1f73c98af8b9a5ae452e9060ca3f1bb17
Kevin Li <kevinli200555@gmail.com>
2025-07-03 11:28:25 -0500

    Updated the rest of the inforamtion after the general instructions part

--- lib/screens/information_screens/video_section_info_screen.dart
+++ lib/screens/information_screens/video_section_info_screen.dart
@@ -23,9 +23,9 @@ class VideoSectionInfoScreen extends StatelessWidget {
       controller: _pageController,
       children: [
         GeneralInstructionsScreen(camera: camera),
-        VideoSectionInfoScreen2(camera: camera),
-        VideoSectionInfoScreen3(camera: camera),
-        VideoSectionInfoScreen4(camera: camera),
+        FewTipsBeforeStart(camera: camera),
+        BeforeYouStartScreen(camera: camera),
+        FilmingTipsScreen(camera: camera),
       ],
     );
   }
@@ -79,6 +79,40 @@ class GeneralInstructionsScreen extends StatelessWidget {
                 ],
               ),
             ),
+            SubTitle(
+              'Swipe Right on your phone to continue reading>>>>',
+              style: TextStyle(color: ColorTheme.red),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class FewTipsBeforeStart extends StatelessWidget {
+  final CameraDescription camera;
+  const FewTipsBeforeStart({super.key, required this.camera});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorTheme.background,
+      appBar: AppBar(
+        title: Text(
+          'Video Section Information Page 2 of 4',
+          style: TextStyle(fontSize: 18),
+        ),
+        centerTitle: true,
+        backgroundColor: ColorTheme.background,
+      ),
+
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             SubTitle('Here are a few tips before you start:'),
             Padding(
               padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
@@ -102,7 +136,7 @@ class GeneralInstructionsScreen extends StatelessWidget {
                   children: [
                     TextSpan(text: '- Try to record in a '),
                     TextSpan(
-                      text: 'quiet',
+                      text: 'quiet ',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                     TextSpan(text: 'place—turn off the TV or music.'),
@@ -191,8 +225,13 @@ class GeneralInstructionsScreen extends StatelessWidget {
                 ),
               ),
             ),
-            BodyText('Thanks again for being part of this—we\'re excited to see what you and your child share with us!\n'),
-            SubTitle('Swipe Right on your phone to continue reading>>>>', style: TextStyle(color: ColorTheme.red))
+            BodyText(
+              'Thanks again for being part of this—we\'re excited to see what you and your child share with us!\n',
+            ),
+            SubTitle(
+              'Swipe Right on your phone to continue reading>>>>',
+              style: TextStyle(color: ColorTheme.red),
+            ),
           ],
         ),
       ),
@@ -200,60 +239,9 @@ class GeneralInstructionsScreen extends StatelessWidget {
   }
 }
 
-class VideoSectionInfoScreen2 extends StatelessWidget {
+class BeforeYouStartScreen extends StatelessWidget {
   final CameraDescription camera;
-  const VideoSectionInfoScreen2({super.key, required this.camera});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorTheme.background,
-      appBar: AppBar(
-        title: Text(
-          'Video Section Information Page 2 of 4',
-          style: TextStyle(fontSize: 18),
-        ),
-        centerTitle: true,
-        backgroundColor: ColorTheme.background,
-        automaticallyImplyLeading: false,
-      ),
-
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SubTitle('Setup'),
-            BodyText(
-              'Make sure your child is not wearing sunglasses or a hat that would obstruct his/her face.\n',
-            ),
-            BodyText(
-              'Please ensure there is no background noise to distract your child (turn off any TV, music, etc)\n',
-            ),
-            BodyText(
-              'Ensure your phone\'s video camera to its highest quality setting with no compression.\n',
-            ),
-            BodyText(
-              'Please use the camera stand included with your package. The phone should be placed '
-              'horizontally (landscape) in the stand using the rear facing (not selfie) camera to record\n',
-            ),
-            BodyText(
-              '(the front facing camera is usually lower quality and could also distract your child.)',
-            ),
-            Image(image: AssetImage('assets/camera_placement_example.png')),
-            SubTitle('\nSWIPE>>>'),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class VideoSectionInfoScreen3 extends StatelessWidget {
-  final CameraDescription camera;
-  const VideoSectionInfoScreen3({super.key, required this.camera});
+  const BeforeYouStartScreen({super.key, required this.camera});
 
   @override
   Widget build(BuildContext context) {
@@ -276,32 +264,102 @@ class VideoSectionInfoScreen3 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SubTitle('Setup (Continued)'),
+            SubTitle('Before You Start'),
             BodyText(
-              'For ease of recording, you could ask a third person to help you record, but please be careful'
-              'this person doesn\'t distract your child with their presence. (But it\'s fine if there\'s a delay'
-              ' between starting the video and walking to the other sideof the room).\n',
+              'Please read through all three tasks before recording. This will help you prepare '
+              'the right items and understand what to say or do during each activity.\n',
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(text: '- Try to follow the steps'),
+                    TextSpan(
+                      text: ' exactly as written,',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          ' especially when calling your child’s name or saying a'
+                          'specific sentence (e.g., “Bring me the toy”). Using the',
+                    ),
+                    TextSpan(
+                      text: ' exact wording ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          'helps us build useful tools like audio-based detection systems.\n',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                          '- If a sentence is included in quotation marks, please say',
+                    ),
+                    TextSpan(
+                      text: ' that exact sentence only once,',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(text: ' as instructed in the task.\n'),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                          '- After you finish the main prompt or question in each task,',
+                    ),
+                    TextSpan(
+                      text: ' keep recording for at least 30 seconds',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: ' or until the interaction ends naturally.\n',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(text: '- Make sure your child’s'),
+                    TextSpan(
+                      text: ' face and body',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          ' are visible as much as possible throughout the video.\n',
+                    ),
+                  ],
+                ),
+              ),
             ),
             BodyText(
-              'Make sure the camera is positioned far enough away that your child\'s full face and body is visible '
-              'during filming. The parent who interacts with child should also be visible during each recorded task.\n',
+              'There are 3 tasks in total. They are meant to feel like playful,'
+              ' everyday moments—so please keep it relaxed and natural.\n',
             ),
-            BodyText(
-              'Please record videos in the daytime when the lighting is sufficient to ensure that the face'
-              'can be clearly seen in the videos. A clear face and body can help us develop better detection models.\n',
+            SubTitle(
+              'Swipe Right on your phone to continue reading>>>>',
+              style: TextStyle(color: ColorTheme.red),
             ),
-            BodyText(
-              'There is no right or wrong answer, just record whatever your child\'s response and actions,'
-              ' even if s/he never responds; this is still very valuable information! Please record the entire interaction.\n',
-            ),
-            BodyText(
-              'You do NOT have to do all tasks at once. Take breaks or record on separate'
-              'days if your child gets tired or loses interest.\n',
-            ),
-            BodyText(
-              'Make sure to enable your camera and microphone when prompted.\n',
-            ),
-            SubTitle('SWIPE>>>'),
           ],
         ),
       ),
@@ -309,16 +367,15 @@ class VideoSectionInfoScreen3 extends StatelessWidget {
   }
 }
 
-class VideoSectionInfoScreen4 extends StatefulWidget {
+class FilmingTipsScreen extends StatefulWidget {
   final CameraDescription camera;
-  const VideoSectionInfoScreen4({super.key, required this.camera});
+  const FilmingTipsScreen({super.key, required this.camera});
 
   @override
-  State<VideoSectionInfoScreen4> createState() =>
-      _VideoSectionInfoScreen4State();
+  State<FilmingTipsScreen> createState() => _FilmingTipsScreenState();
 }
 
-class _VideoSectionInfoScreen4State extends State<VideoSectionInfoScreen4> {
+class _FilmingTipsScreenState extends State<FilmingTipsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -340,26 +397,82 @@ class _VideoSectionInfoScreen4State extends State<VideoSectionInfoScreen4> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SubTitle('Task Instructions'),
-            BodyText(
-              'Read through each task before recording it to ensure you have the right props and know how to respond.\n',
+            SubTitle('Filming Tips'),
+            RichBodyText(
+              TextSpan(
+                children: [
+                  TextSpan(text: 'Try to keep your child’s'),
+                  TextSpan(
+                    text: ' full body and face',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text:
+                        'visible in the video whenever possible. You can choose one of'
+                        'the following ways to record, based on what works best for your situation:\n',
+                  ),
+                ],
+              ),
             ),
-            BodyText(
-              'Please try to follow each procedure exactly. (e.g. Don\'t call your child\'s name or repeat'
-              ' phrases more than what is described in the task.) \n',
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '- Hold the phone yourself',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          ' (recommended if possible): If filming alone, you can hold the phone'
+                          ' casually during the task—just try to be natural and avoid making the '
+                          'child feel like they are being “watched.”\n',
+                    ),
+                  ],
+                ),
+              ),
             ),
-            BodyText(
-              'If you are asked to say a sentence, for example, "Bring me the bear", then please'
-              'speak the exact same sentence (this will help us design a cool audio detection tool!).\n',
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '- Place the phone on a stable surface:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          ' If your hands are needed for part of the task (e.g., using a'
+                          ' spoon), you can briefly place the phone somewhere steady. '
+                          'Just make sure the angle captures your child’s body and face.\n',
+                    ),
+                  ],
+                ),
+              ),
             ),
-            BodyText(
-              'After you complete the verbal portion of each task, please continue to record your child'
-              '(and interact with them) for at least 30 seconds more before stopping each recording.'
-              '(Note task 7 is a little different).\n\n\n',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(16, 0, 0, 0),
+              child: RichBodyText(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '- Have a second person film:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          ' This helps you focus on interacting with your child while someone else'
+                          'holds the phone and follows the child’s movement.\n',
+                    ),
+                  ],
+                ),
+              ),
             ),
-            BodyText(
-              'When you are ready to begin the test, click the below button\n',
+            SubTitle(
+              'Swipe Left on your phone to reread <<<<\n\n',
+              style: TextStyle(color: ColorTheme.red),
             ),
             NextButton(
               label: 'BEGIN TEST',
