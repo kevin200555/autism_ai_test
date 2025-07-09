ad82942027415a4cb89103be6ad36c31fc3be75b
Kevin Li <kevinli200555@gmail.com>
2025-07-09 10:48:08 -0500

    Added a function that screenshots the signiture page This, way I can easily get the signiture of the user and save them to a file

--- lib/screens/data_gathering/ic_signiture.dart
+++ lib/screens/data_gathering/ic_signiture.dart
@@ -12,6 +12,11 @@ import 'package:autism_ai_test/widgets/signiture_question_widget.dart';
 import 'package:autism_ai_test/widgets/text_types.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'dart:typed_data';
+import 'dart:io';
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
 
 class InformedConsentSignitureScreen extends StatefulWidget {
   final CameraDescription camera;
@@ -26,43 +31,77 @@ class _InformedConsentSignitureScreenState
     extends State<InformedConsentSignitureScreen> {
   var signitureQuestions = InstructionAndQuestions.getSigniture();
   var userId = '';
+  final GlobalKey _screenShotKey = GlobalKey();
   //List<String?> responses = [];
   //Honestly not sure how I should handle responses here, I think I'll just wait
+
   @override
   void initState() {
     super.initState();
     //responses = List<String?>.filled(signitureQuestions.length, null);
   }
 
+  Future<void> screenShot() async {
+    try {
+      RenderRepaintBoundary boundary =
+          _screenShotKey.currentContext!.findRenderObject()
+              as RenderRepaintBoundary;
+
+      // Capture image with higher resolution
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+
+      ByteData? byteData = await image.toByteData(
+        format: ui.ImageByteFormat.png,
+      );
+      Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+      // Get app directory to save the file
+      final directory = await getApplicationDocumentsDirectory();
+      final filePath =
+          '${directory.path}/screenshot_${DateTime.now().millisecondsSinceEpoch}.png';
+
+      final file = File(filePath);
+      await file.writeAsBytes(pngBytes);
+
+      print('Screenshot saved to $filePath');
+    } catch (e) {
+      print('Error saving screenshot: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorTheme.background,
-      appBar: AppBar(
-        title: AppBarTitle('SIGNITURE', color: ColorTheme.alternateTextColor),
-        centerTitle: true,
-        backgroundColor: ColorTheme.accent,
-        iconTheme: IconThemeData(color: ColorTheme.alternateTextColor),
-      ),
+    return RepaintBoundary(
+      key: _screenShotKey,
+      child: Scaffold(
+        backgroundColor: ColorTheme.background,
+        appBar: AppBar(
+          title: AppBarTitle('SIGNITURE', color: ColorTheme.alternateTextColor),
+          centerTitle: true,
+          backgroundColor: ColorTheme.accent,
+          iconTheme: IconThemeData(color: ColorTheme.alternateTextColor),
+        ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          DrawingQuestionWidget(question: signitureQuestions[0][1]),
-          DrawingQuestionWidget(question: signitureQuestions[1][1]),
-          NextButton(
-            label: 'NEXT',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      VideoSectionInfoScreen(camera: widget.camera),
-                ),
-              );
-            },
-          ),
-        ],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            DrawingQuestionWidget(question: signitureQuestions[0][1]),
+            DrawingQuestionWidget(question: signitureQuestions[1][1]),
+            NextButton(
+              label: 'SUBMIT SIGNITURES',
+              onPressed: () {
+                screenShot();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        VideoSectionInfoScreen(camera: widget.camera),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
