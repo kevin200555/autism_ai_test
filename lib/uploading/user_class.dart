b7e434dc6506de14135ed09c87eb5fef136525b6
Kevin Li <kevinli200555@gmail.com>
2025-07-11 11:43:52 -0500

    added a debug mode

--- lib/uploading/user_class.dart
+++ lib/uploading/user_class.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:flutter/foundation.dart';
 import 'package:camera/camera.dart';
 
 class UserClass {
@@ -8,17 +8,19 @@ class UserClass {
   static File? signiture;
   static List<String?>? mChatRresponses;
   static List<String?>? intakeResponses;
-  static List<XFile?>? recordedVideos = [null,null,null];
+  static List<XFile?>? recordedVideos = [null, null, null];
 
   static void printSummary() {
-    print("User ID: $userId");
-    print("Informed Consent Responses: $iCResponses");
-    print("Signiture FilePath: ${signiture?.path}");
-    print("mChatR Responses: $mChatRresponses");
-    print("Intake Form Responses: $intakeResponses");
+    if (kDebugMode) {
+      print("User ID: $userId");
+      print("Informed Consent Responses: $iCResponses");
+      print("Signiture FilePath: ${signiture?.path}");
+      print("mChatR Responses: $mChatRresponses");
+      print("Intake Form Responses: $intakeResponses");
 
-    print("Recorded Video 1: ${recordedVideos?[0]?.path}");
-    print("Recorded Video 2: ${recordedVideos?[1]?.path}");
-    print("Recorded Video 3: ${recordedVideos?[2]?.path}");
+      print("Recorded Video 1: ${recordedVideos?[0]?.path}");
+      print("Recorded Video 2: ${recordedVideos?[1]?.path}");
+      print("Recorded Video 3: ${recordedVideos?[2]?.path}");
+    }
   }
 } // EOF user
