77a223a9039a95905ace9827c4ac43deb6927b51
Kevin Li <kevinli200555@gmail.com>
2025-07-23 10:35:03 -0500

    started documenting the widgets

--- lib/widgets/next_button.dart
+++ lib/widgets/next_button.dart
@@ -1,8 +1,10 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:autism_ai_test/constants/colors.dart';
-
+// This class creates a next button that is reusable throughout all screens
+// It's purpose is to direct users to a new screen
 class NextButton extends StatelessWidget {
+  // changable button lable name as well as a changable onPressed function
   final String label;
   final VoidCallback onPressed;
 
@@ -10,13 +12,17 @@ class NextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // creates a boundary area for the button
     return SizedBox(
       width: double.infinity,
       height: 56,
+      // keeps the button away from the edges
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        // Actual Button widget
         child: ElevatedButton.icon(
           onPressed: onPressed,
+          // Button Label (doesn't have to say 'next')
           label: AutoSizeText(
             label,
             style: TextStyle(
--- lib/widgets/signiture_question_widget.dart
+++ lib/widgets/signiture_question_widget.dart
@@ -1,8 +1,9 @@
 import 'package:autism_ai_test/constants/colors.dart';
 import 'package:autism_ai_test/widgets/text_types.dart';
 import 'package:flutter/material.dart';
-// This Widget is used to draw out answers to a given question
-// in this program it is used for the user to write signiture
+
+// This class' creates a widget that is meant to get some drawn response from the user
+// It is used to get user's signitures
 class DrawingQuestionWidget extends StatefulWidget {
   final String question;
   final Function(bool) onChanged;
@@ -20,29 +21,34 @@ class DrawingQuestionWidget extends StatefulWidget {
 
 class _DrawingQuestionWidgetState extends State<DrawingQuestionWidget> {
   List<Offset?> points = [];
+  // saves the drawn answers (like the controller for short answer question)
   final GlobalKey _canvasKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
+    // sets a bounding box for this widget
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      // returns a coloum of the quesetion on top, followed an answer box where the user can draw/erase their answer
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // question text
           QuestionText(widget.question),
           const SizedBox(height: 10),
+          // box that makes the answer box
           GestureDetector(
             onPanUpdate: (details) {
               final box = _canvasKey.currentContext?.findRenderObject();
               if (box is RenderBox) {
                 final localPosition = box.globalToLocal(details.globalPosition);
-
+                // sets bounds and position of cursor
                 final withinBounds =
                     localPosition.dy >= 0 &&
                     localPosition.dy <= box.size.height &&
                     localPosition.dx >= 0 &&
                     localPosition.dx <= box.size.width;
-
+                // updates if cursor is within the bounds
                 if (withinBounds) {
                   widget.onChanged(true);
                   setState(() => points.add(localPosition));
@@ -62,6 +68,7 @@ class _DrawingQuestionWidgetState extends State<DrawingQuestionWidget> {
             ),
           ),
           const SizedBox(height: 10),
+          // Row that contains the button which will erase the answer box
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -92,24 +99,29 @@ class _DrawingQuestionWidgetState extends State<DrawingQuestionWidget> {
   }
 }
 
+// CustomPainter that draws the user's signature or drawing on the canvas
 class DrawingPainter extends CustomPainter {
   final List<Offset?> points;
   DrawingPainter(this.points);
 
   @override
   void paint(Canvas canvas, Size size) {
+    // Set up the paint style for drawing lines
     final paint = Paint()
       ..color = ColorTheme.textColor
       ..strokeWidth = 4.0
       ..strokeCap = StrokeCap.round;
 
+    // Iterate through the list of points and draw lines between consecutive points
     for (int i = 0; i < points.length - 1; i++) {
+      // Only draw if both points are not null (null indicates a break in the drawing)
       if (points[i] != null && points[i + 1] != null) {
         canvas.drawLine(points[i]!, points[i + 1]!, paint);
       }
     }
   }
 
+  // Always repaint when notified, since the points may have changed
   @override
   bool shouldRepaint(DrawingPainter oldDelegate) => true;
 } // EOF signiture_question_widget.dart
