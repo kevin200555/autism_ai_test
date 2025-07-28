2628d48540c5a6fffc317f02754a839e9e65b97b
Kevin Li <kevinli200555@gmail.com>
2025-07-28 11:15:19 -0500

    updated progress bar to check for overflow

--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -3,7 +3,7 @@ import 'package:autism_ai_test/uploading/user_class.dart';
 import 'package:flutter/material.dart';
 
 // This class introduces a widget that displays to the user how far along they are in their test
-// 
+//
 class ProgressBar extends StatefulWidget {
   const ProgressBar({super.key});
 
@@ -20,6 +20,7 @@ class _ProgressBarState extends State<ProgressBar> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          // the progress bar is made up of a bottom rectangle and a top green rectangle stacked on top of it.
           Stack(
             children: [
               // Bottom rectangle
@@ -31,9 +32,23 @@ class _ProgressBarState extends State<ProgressBar> {
 
               // Top rectangle (shows actual progress)
               Container(
-                width: MediaQuery.sizeOf(context).width * 0.65 * (UserClass.screenNumber/UserClass.totalScreenNumber),
+                width:
+                    // if user ever somhow gets to 14/13 or something, this makes sure the top rectangle doesn't get any bigger
+                    (UserClass.screenNumber / UserClass.totalScreenNumber) >= 1
+                    ? MediaQuery.sizeOf(context).width * 0.65
+                    : MediaQuery.sizeOf(context).width *
+                          0.65 *
+                          (UserClass.screenNumber /
+                              UserClass.totalScreenNumber),
                 height: MediaQuery.sizeOf(context).height * 0.02,
-                color: ColorTheme.green,
+                color:
+                    (UserClass.screenNumber / UserClass.totalScreenNumber) <
+                        0.333
+                    ? ColorTheme.progressBar1
+                    : (UserClass.screenNumber / UserClass.totalScreenNumber) <
+                          0.666
+                    ? ColorTheme.progressBar2
+                    : ColorTheme.progressBar3,
               ),
             ],
           ),
@@ -41,8 +56,13 @@ class _ProgressBarState extends State<ProgressBar> {
           SizedBox(width: MediaQuery.sizeOf(context).width * 0.05),
           // Displays percent of exam completed
           Text(
-            //'${UserClass.screenNumber}',
-            '${(100*(UserClass.screenNumber/UserClass.totalScreenNumber)).toInt()}% Done',
+            // not sure why it decided to format it like this but basically if the user ever accidently
+            // somehow gets to 14/13 or something, this makes sure that number stays at 100%
+            (100 * (UserClass.screenNumber / UserClass.totalScreenNumber))
+                        .toInt() <=
+                    100
+                ? '${(100 * (UserClass.screenNumber / UserClass.totalScreenNumber)).toInt()}% Done'
+                : '100% Done',
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
