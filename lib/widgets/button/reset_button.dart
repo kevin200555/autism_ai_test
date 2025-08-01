a25ce54f7497e95d79462ae0c3ac618b5d3b3cfb
Kevin Li <kevinli200555@gmail.com>
2025-08-01 09:59:17 -0500

    designed and implemented the reset button

--- lib/widgets/button/reset_button.dart
+++ lib/widgets/button/reset_button.dart
@@ -1,6 +1,7 @@
 import 'package:autism_ai_test/constants/colors.dart';
 import 'package:autism_ai_test/screens/information_screens/home_screen.dart';
 import 'package:autism_ai_test/uploading/user_class.dart';
+import 'package:autism_ai_test/widgets/text_types.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -13,19 +14,25 @@ class ResetButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.help_outline),
-      // sets size 
-      iconSize: MediaQuery.of(context).size.width * 0.08, 
-      color: ColorTheme.textColor,
-      onPressed: () {
-        UserClass.resetAll();
-        
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => HomeScreen(camera: camera,)),
-        );
-      },
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: ElevatedButton.icon(
+        onPressed: () {
+          UserClass.resetAll();
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen(camera: camera)),
+          );
+        },
+        label: ButtonText('RESET PROGRESS', maxLines: 1),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ColorTheme.red,
+          foregroundColor: ColorTheme.alternateTextColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          padding: const EdgeInsets.symmetric(vertical: 14),
+        ),
+      ),
     );
   }
 } // EOF help_button.dart
