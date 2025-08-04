91194cfb8f2b9931608333f3ad136ad6879e1fda
Kevin Li <kevinli200555@gmail.com>
2025-08-04 12:27:09 -0500

    documented upload_to_firebase

--- lib/uploading/upload_to_firebase.dart
+++ lib/uploading/upload_to_firebase.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-
+// The purpose of this module is to upload the data to firebase
+// The data is first compressed into a zip file, found by this module,
+// and then uploaded to firebase
+// It is important to note that I made it so that only the admin can read the data, anyone else cannot
 Future<String?> uploadFile(String filePath) async {
   
   if (kDebugMode) {
     print('Uploading file: $filePath');
   }
+  // find the filePath that is attempting to be uploaded
   final file = File(filePath);
   if (!await file.exists()) {
     if (kDebugMode) {
