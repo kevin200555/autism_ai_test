e43fc4ca2762f2c949de972012ab24c90f81caeb
Kevin Li <kevinli200555@gmail.com>
2025-08-15 14:22:23 -0500

    added a way to store the total length of videos recorded

--- lib/uploading/video_storage_class.dart
+++ lib/uploading/video_storage_class.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:autism_ai_test/constants/instruction_and_questions.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoStorageClassItem {
+  static bool isActive = false;
   static String date = '';
   static String time = '';
-  static int timeElasped = 0;
+  static String timeElasped = '';
   // These store the videos (size of these could change later)
   static List<XFile?>? recordedVideos = List<XFile?>.filled(
     InstructionAndQuestions.videoNames.length,
@@ -14,8 +18,22 @@ class VideoStorageClassItem {
     growable: false,
   );
 
+  static resetAll() {
+    isActive = false;
+    date = '';
+    time = '';
+    timeElasped = '';
+    recordedVideos = List<XFile?>.filled(
+      InstructionAndQuestions.videoNames.length,
+      null,
+      growable: false,
+    );
+    saveToHive();
+  }
+
   static Future<void> saveToHive() async {
     final box = await Hive.openBox('user_data');
+    await box.put('isActive', date);
     await box.put('date', date);
     await box.put('time', time);
     await box.put('timeElasped', timeElasped);
@@ -28,6 +46,7 @@ class VideoStorageClassItem {
 
   static Future<void> loadFromHive() async {
     final box = await Hive.openBox('user_data');
+    isActive = box.get('isActive');
     date = box.get('date');
     time = box.get('time');
     timeElasped = box.get('timeElasped');
@@ -44,6 +63,7 @@ class VideoStorageClassItem {
   // prints all relevant variables in the UserClass
   static void printSummary() {
     if (kDebugMode) {
+      print(isActive);
       print(date);
       print(time);
       print(timeElasped);
@@ -53,10 +73,37 @@ class VideoStorageClassItem {
     }
   }
 
-  static void getTime() {
+  static void startVideoRecording() {
+    isActive = true;
+    saveToHive();
+  }
+
+  static void getStartTime() {
     DateTime now = DateTime.now();
     date = "${now.month}-${now.day}-${now.year}";
     time = "${now.hour}:${now.minute}";
     saveToHive();
+  }
+
+  Future<void> getTotalVideoDuration(List<XFile?>? videos) async {
+    if (videos == null) timeElasped = "0:00";
+
+    Duration totalDuration = Duration.zero;
+
+    for (XFile? video in videos!) {
+      if (video == null) continue;
+
+      VideoPlayerController controller = VideoPlayerController.file(
+        File(video.path),
+      );
+      await controller.initialize(); // Load metadata
+      totalDuration += controller.value.duration;
+      await controller.dispose(); // Free resources
+    }
+
+    int minutes = totalDuration.inMinutes;
+    int seconds = totalDuration.inSeconds % 60;
+
+    timeElasped = "$minutes:${seconds.toString().padLeft(2, '0')}";
   }
 }
