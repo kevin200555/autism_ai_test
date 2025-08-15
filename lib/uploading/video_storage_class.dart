8d4ba009907b20bb5a4269794c78f0fe2e8f4044
Kevin Li <kevinli200555@gmail.com>
2025-08-15 13:55:12 -0500

    essentinally copied a few of the methods from the user class and put them into the video one. Addd additional method that takes the current date and time

--- lib/uploading/video_storage_class.dart
+++ lib/uploading/video_storage_class.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:autism_ai_test/constants/instruction_and_questions.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
@@ -8,6 +6,7 @@ import 'package:hive/hive.dart';
 class VideoStorageClassItem {
   static String date = '';
   static String time = '';
+  static int timeElasped = 0;
   // These store the videos (size of these could change later)
   static List<XFile?>? recordedVideos = List<XFile?>.filled(
     InstructionAndQuestions.videoNames.length,
@@ -17,14 +16,21 @@ class VideoStorageClassItem {
 
   static Future<void> saveToHive() async {
     final box = await Hive.openBox('user_data');
+    await box.put('date', date);
+    await box.put('time', time);
+    await box.put('timeElasped', timeElasped);
     // Save recorded video paths
     List<String?> videoPaths = recordedVideos!
         .map((file) => file?.path)
         .toList();
     await box.put('recordedVideoPaths', videoPaths);
   }
+
   static Future<void> loadFromHive() async {
     final box = await Hive.openBox('user_data');
+    date = box.get('date');
+    time = box.get('time');
+    timeElasped = box.get('timeElasped');
     final videoPaths = (box.get('recordedVideoPaths') as List?)
         ?.cast<String?>();
     if (videoPaths != null) {
@@ -38,9 +44,19 @@ class VideoStorageClassItem {
   // prints all relevant variables in the UserClass
   static void printSummary() {
     if (kDebugMode) {
+      print(date);
+      print(time);
+      print(timeElasped);
       for (int i = 0; i < InstructionAndQuestions.videoNames.length; i++) {
         print("Recorded Video 1: ${recordedVideos?[i]?.path}");
       }
     }
+  }
+
+  static void getTime() {
+    DateTime now = DateTime.now();
+    date = "${now.year}-${now.month}-${now.day}";
+    time = "${now.hour}:${now.minute}:${now.second}";
+    saveToHive();
   }
 }
