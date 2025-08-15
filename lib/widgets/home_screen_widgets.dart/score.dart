20e10211e1051c73d1ddf91f4a1b03f48df09e04
Kevin Li <kevinli200555@gmail.com>
2025-08-15 11:42:10 -0500

    The Score is a circular progress bar that displays to the user the chance that their child has autism (given that it works)

---
+++ lib/widgets/home_screen_widgets.dart/score.dart
@@ -0,0 +1,64 @@
+import 'package:autism_ai_test/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class PartialCircle extends StatelessWidget {
+  final double fraction;
+  final double size;
+  final Color color;
+
+  const PartialCircle({
+    super.key,
+    required this.fraction,
+    required this.size,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Text(
+          '${(fraction * 100).toInt()}%',
+          style: TextStyle(
+            fontSize: 25,
+            color: ColorTheme.textColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        CustomPaint(
+          size: Size(size, size),
+          painter: _PartialCirclePainter(fraction, color),
+        ),
+      ],
+    );
+  }
+}
+
+class _PartialCirclePainter extends CustomPainter {
+  final double fraction;
+  final Color color;
+
+  _PartialCirclePainter(this.fraction, this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8;
+
+    canvas.drawArc(
+      rect,
+      -pi / 2, // start at top
+      2 * pi * fraction,
+      false,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
