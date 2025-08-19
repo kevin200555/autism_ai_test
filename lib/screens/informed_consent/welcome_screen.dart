919089c67f5e22e1f5beede3795dd27b7f69e30a
Kevin Li <kevinli200555@gmail.com>
2025-08-19 11:11:13 -0500

    comments on welcome_screen

--- lib/screens/informed_consent/welcome_screen.dart
+++ lib/screens/informed_consent/welcome_screen.dart
@@ -6,6 +6,10 @@ import 'package:autism_ai_test/widgets/other/text_types.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
+// This screen is the screen that the user would see first before doing anything else on the app
+// It serves as a welcome screen and purpose is to explain to the user that we need signiture first before 
+// continuing with the rest of the app
+// welcome_screen -> ic_document -> signing and forms -> main_menu -> branches to forms, settings, video recording, etc.
 class WelcomeScreen extends StatelessWidget {
   final CameraDescription camera;
 
