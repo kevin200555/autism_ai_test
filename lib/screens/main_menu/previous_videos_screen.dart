03c4016f7976ee923e31f2f1e2e8d7756cfe1918
Kevin Li <kevinli200555@gmail.com>
2025-08-21 13:44:22 -0500

    Added some text to that previous_videos_screen

--- lib/screens/main_menu/previous_videos_screen.dart
+++ lib/screens/main_menu/previous_videos_screen.dart
@@ -1,30 +1,40 @@
+import 'package:autism_ai_test/constants/colors.dart';
 import 'package:autism_ai_test/uploading/user_class.dart';
 import 'package:autism_ai_test/widgets/home_screen_widgets.dart/previous_videos_tile.dart';
+import 'package:autism_ai_test/widgets/other/text_types.dart';
 import 'package:flutter/material.dart';
 
 class PreviousVideosScreen extends StatelessWidget {
   const PreviousVideosScreen({super.key});
 
-  // Sample list of video data
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: UserClass.videoList.length,
-        itemBuilder: (context, index) {
-          final video = UserClass.videoList[index];
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 12),
-            child: PreviousVideosTile(
-              date: video.date,
-              time: video.time,
-              score: '47',
+      body: Scrollbar(
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            // Your text at the top
+            SubTitle('Previous recordings:'),
+            BodyText(
+              'Here is a list of your previous recording sessions'
+              'You can click on each to view more information about them.\n',
+              color: ColorTheme.textColor,
             ),
-          );
-        },
+
+            // Now your list of tiles
+            ...UserClass.videoList.map(
+              (video) => Padding(
+                padding: EdgeInsets.fromLTRB(16,4,16,4),
+                child: PreviousVideosTile(
+                  date: video.date,
+                  time: video.time,
+                  score: '47',
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
